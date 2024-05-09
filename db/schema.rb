5488170d4a9c78ba121c82ed42d6d4d03d648a41
vidya <vidyanerusu@gmail.com>
2024-05-09 18:34:21 -0500

    Tagging feature completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_000242) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_30_222810) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -27,5 +27,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_15_000242) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "comments", "articles"
+  add_foreign_key "taggings", "articles"
+  add_foreign_key "taggings", "tags"
 end
